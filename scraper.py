2c3450a73ee91eed4f25a9a8f2bbd6f0ed533138
Kae Ancheta <kae.ancheta34@gmail.com>
2020-02-07 16:12:06 -0800

    scraper

--- scraper.py
+++ scraper.py
@@ -1,18 +1,57 @@
 import re
 from urllib.parse import urlparse
+from lxml import html
+import requests
+from lxml import etree, objectify
+
 
 def scraper(url, resp):
+    result = []
+    page = requests.get(url)
+    if page.status_code != 200:
+        return result
     links = extract_next_links(url, resp)
-    return [link for link in links if is_valid(link)]
+    result = [link for link in links if is_valid(link)]
+    return result
+
+def getURL(page):
+    start_link = page.find("a href")
+    if start_link == -1:
+        return None, 0
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1: end_quote]
+    return url, end_quote
 
 def extract_next_links(url, resp):
     # Implementation requred.
-    return list()
-#Additional rules should be added to the is_valid function to filter the urls
+    result = []
+    page = requests.get(url)
+    if page.status_code != 200:
+        return result
+    page = str(page.content)
+    while True:
+        url, n = getURL(page)
+        page = page[n:]
+        if url:
+            if '#' not in url:
+                p = requests.get(url)
+                if p.status_code != 200:
+                    return result
+            result.append(url)
+        else:
+            break
+    return result
+
 def is_valid(url):
     try:
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
+            return False
+        domains = set(["ics.uci.edu", "cs.uci.edu", "informatics.uci.edu", "stat.uci.edu", "today.uci.edu/department/information_computer_sciences"])
+        if not any(d in url for d in domains):
+            return False
+        if "http://http://" in url:
             return False
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
